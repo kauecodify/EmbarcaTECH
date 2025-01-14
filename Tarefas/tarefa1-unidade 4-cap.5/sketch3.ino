491e87f534a1a29daa44bfb3be71c9d4268a5aba
kauecodify <143859403+kauecodify@users.noreply.github.com>
2025-01-14 01:05:59 -0300

    Create sketch3.ino

---
+++ Tarefas/tarefa1-unidade 4-cap.5/sketch3.ino
@@ -0,0 +1,33 @@
+#include "WiFi.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Initializing WiFi...");
+  WiFi.mode(WIFI_STA);
+  Serial.println("Setup done!");
+}
+
+void loop() {
+  Serial.println("Scanning...");
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan done!");
+  if (n == 0) {
+    Serial.println("No networks found.");
+  } else {
+    Serial.println();
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  delay(5000);
+}
